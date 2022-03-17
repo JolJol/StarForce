6a13efa58e91257f4daca6596925ecbf633be5f0
zhaozhenyu <zhaozhenyu@Kunpo.cc>
2022-03-17 19:16:28 +0800

    1

---
+++ Assets/GameMain/Scripts/AIFightTest/AIFight.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameMain.Scripts.AIFightTest
+{
+    public class AiFight : MonoBehaviour
+    {
+        public AiState AiState;
+        public float IdleTimer = 0;
+        public float IdleTime = 1;
+        public float AttackTimer = 0;
+        public float AttackTime = 1;
+        private void Start()
+        {
+            AiState = AiState.Idle;
+            IdleTimer = Time.time + IdleTime;
+        }
+        
+        private void Update()
+        {
+            switch (AiState)
+            {
+                case AiState.Idle:
+                    if (Time.time > IdleTimer)
+                    {
+                        AiState = AiState.FindTarget;
+                    }
+                    break;
+                case AiState.FindTarget:
+                    
+                    break;
+                case AiState.Fight:
+                    break;
+            }
+        }
+    }
+
+    public enum AiState
+    {
+        /// <summary>
+        /// 闲置
+        /// </summary>
+        Idle,
+
+        /// <summary>
+        /// 寻找目标
+        /// </summary>
+        FindTarget,
+
+        /// <summary>
+        /// 向目标寻路
+        /// </summary>
+        NavigateTarget,
+        
+        /// <summary>
+        /// 战斗
+        /// </summary>
+        Fight,
+    }
+}
---
+++ Assets/GameMain/Scripts/AIFightTest/EnemyManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using StarForce;
+using UnityEngine;
+
+namespace GameMain.Scripts.AIFightTest
+{
+    public class EnemyManager:MonoBehaviour
+    {
+        private EnemyManager m_Instance;
+        private List<Transform> m_Enemies;
+
+        public GameObject EnemyPrefab;
+
+        public EnemyManager Instance
+        {
+            get => m_Instance;
+            set => m_Instance = value;
+        }
+
+        private void Awake()
+        {
+            m_Instance = this;
+            m_Enemies = new List<Transform>();
+        }
+
+        private void CreateEnmey()
+        {
+            
+        }
+        
+        public Transform FindEnemyTarget(Vector3 pos)
+        {
+            Transform target = null;
+            float minDis = Mathf.Infinity;
+            foreach (var enemy in m_Enemies)
+            {
+                float dis = Vector3.Distance(pos, enemy.position);
+                if (dis < minDis)
+                {
+                    minDis = dis;
+                    target = enemy;
+                }
+            }
+            return target;
+        }
+    }
+}
